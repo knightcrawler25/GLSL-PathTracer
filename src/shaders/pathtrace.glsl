0f9e0ee712c7d3288dd0dd16788496441b8e6077
knightcrawler25 <knightcrawler25@gmail.com>
2022-01-06 15:40:24 +0530

    Disable NEE

--- src/shaders/pathtrace.glsl
+++ src/shaders/pathtrace.glsl
@@ -71,17 +71,6 @@ struct Camera
     float fov;
 };
 
-struct Light
-{
-    vec3 position;
-    vec3 emission;
-    vec3 u;
-    vec3 v;
-    float radius;
-    float area;
-    float type;
-};
-
 struct State
 {
     int depth;
@@ -90,8 +79,6 @@ struct State
     vec3 fhp;
     vec3 normal;
     vec3 ffnormal;
-
-    bool isEmitter;
 
     int matID;
     Material mat;
@@ -104,14 +91,6 @@ struct BsdfSampleRec
     float pdf;
 };
 
-struct LightSampleRec
-{
-    vec3 normal;
-    vec3 emission;
-    vec3 direction;
-    float dist;
-    float pdf;
-};
 
 uniform Camera camera;
 
@@ -137,28 +116,6 @@ float rand()
     pcg4d(seed); return float(seed.x) / float(0xffffffffu);
 }
 
-float RectIntersect(in vec3 pos, in vec3 u, in vec3 v, in vec4 plane, in Ray r)
-{
-    vec3 n = vec3(plane);
-    float dt = dot(r.direction, n);
-    float t = (plane.w - dot(n, r.origin)) / dt;
-
-    if (t > EPS)
-    {
-        vec3 p = r.origin + r.direction * t;
-        vec3 vi = p - pos;
-        float a1 = dot(u, vi);
-        if (a1 >= 0.0 && a1 <= 1.0)
-        {
-            float a2 = dot(v, vi);
-            if (a2 >= 0.0 && a2 <= 1.0)
-                return t;
-        }
-    }
-
-    return INF;
-}
-
 float AABBIntersect(vec3 minCorner, vec3 maxCorner, Ray r)
 {
     vec3 invDir = 1.0 / r.direction;
@@ -175,45 +132,10 @@ float AABBIntersect(vec3 minCorner, vec3 maxCorner, Ray r)
     return (t1 >= t0) ? (t0 > 0.f ? t0 : t1) : -1.0;
 }
 
-bool ClosestHit(Ray r, inout State state, inout LightSampleRec lightSampleRec)
+bool ClosestHit(Ray r, inout State state)
 {
     float t = INF;
     float d;
-
-    for (int i = 0; i < numOfLights; i++)
-    {
-        // Fetch light Data
-        vec3 position = texelFetch(lightsTex, ivec2(i * 5 + 0, 0), 0).xyz;
-        vec3 emission = texelFetch(lightsTex, ivec2(i * 5 + 1, 0), 0).xyz;
-        vec3 u = texelFetch(lightsTex, ivec2(i * 5 + 2, 0), 0).xyz;
-        vec3 v = texelFetch(lightsTex, ivec2(i * 5 + 3, 0), 0).xyz;
-        vec3 params = texelFetch(lightsTex, ivec2(i * 5 + 4, 0), 0).xyz;
-        float radius = params.x;
-        float area = params.y;
-        float type = params.z;
-
-        if (type == QUAD_LIGHT)
-        {
-            vec3 normal = normalize(cross(u, v));
-            if (dot(normal, r.direction) > 0.) // Hide backfacing quad light
-                continue;
-            vec4 plane = vec4(normal, dot(normal, position));
-            u *= 1.0f / dot(u, u);
-            v *= 1.0f / dot(v, v);
-
-            d = RectIntersect(position, u, v, plane, r);
-            if (d < 0.)
-                d = INF;
-            if (d < t)
-            {
-                t = d;
-                float cosTheta = dot(-r.direction, normal);
-                lightSampleRec.pdf = (t * t) / (area * cosTheta);
-                lightSampleRec.emission = emission;
-                state.isEmitter = true;
-            }
-        }
-    }
 
     // Intersect BVH and tris
     int stack[64];
@@ -356,8 +278,6 @@ bool ClosestHit(Ray r, inout State state, inout LightSampleRec lightSampleRec)
 
     if (triID.x != -1)
     {
-        state.isEmitter = false;
-
         vec4 n0 = texelFetch(normalsTex, triID.x);
         vec4 n1 = texelFetch(normalsTex, triID.y);
         vec4 n2 = texelFetch(normalsTex, triID.z);
@@ -375,156 +295,6 @@ bool ClosestHit(Ray r, inout State state, inout LightSampleRec lightSampleRec)
     return true;
 }
 
-bool AnyHit(Ray r, float maxDist)
-{
-    // Intersect Emitters
-    for (int i = 0; i < numOfLights; i++)
-    {
-        // Fetch light Data
-        vec3 position = texelFetch(lightsTex, ivec2(i * 5 + 0, 0), 0).xyz;
-        vec3 emission = texelFetch(lightsTex, ivec2(i * 5 + 1, 0), 0).xyz;
-        vec3 u = texelFetch(lightsTex, ivec2(i * 5 + 2, 0), 0).xyz;
-        vec3 v = texelFetch(lightsTex, ivec2(i * 5 + 3, 0), 0).xyz;
-        vec3 params = texelFetch(lightsTex, ivec2(i * 5 + 4, 0), 0).xyz;
-        float radius = params.x;
-        float area = params.y;
-        float type = params.z;
-
-        // Intersect rectangular area light
-        if (type == QUAD_LIGHT)
-        {
-            vec3 normal = normalize(cross(u, v));
-            vec4 plane = vec4(normal, dot(normal, position));
-            u *= 1.0f / dot(u, u);
-            v *= 1.0f / dot(v, v);
-
-            float d = RectIntersect(position, u, v, plane, r);
-            if (d > 0.0 && d < maxDist)
-                return true;
-        }
-    }
-
-    // Intersect BVH and tris
-    int stack[64];
-    int ptr = 0;
-    stack[ptr++] = -1;
-
-    int index = topBVHIndex;
-    float leftHit = 0.0;
-    float rightHit = 0.0;
-    bool BLAS = false;
-
-    Ray rTrans;
-    rTrans.origin = r.origin;
-    rTrans.direction = r.direction;
-
-    while (index != -1)
-    {
-        ivec3 LRLeaf = ivec3(texelFetch(BVH, index * 3 + 2).xyz);
-
-        int leftIndex = int(LRLeaf.x);
-        int rightIndex = int(LRLeaf.y);
-        int leaf = int(LRLeaf.z);
-
-        if (leaf > 0) // Leaf node of BLAS
-        {
-            for (int i = 0; i < rightIndex; i++) // Loop through tris
-            {
-                ivec3 vertIndices = ivec3(texelFetch(vertexIndicesTex, leftIndex + i).xyz);
-
-                vec4 v0 = texelFetch(verticesTex, vertIndices.x);
-                vec4 v1 = texelFetch(verticesTex, vertIndices.y);
-                vec4 v2 = texelFetch(verticesTex, vertIndices.z);
-
-                vec3 e0 = v1.xyz - v0.xyz;
-                vec3 e1 = v2.xyz - v0.xyz;
-                vec3 pv = cross(rTrans.direction, e1);
-                float det = dot(e0, pv);
-
-                vec3 tv = rTrans.origin - v0.xyz;
-                vec3 qv = cross(tv, e0);
-
-                vec4 uvt;
-                uvt.x = dot(tv, pv);
-                uvt.y = dot(rTrans.direction, qv);
-                uvt.z = dot(e1, qv);
-                uvt.xyz = uvt.xyz / det;
-                uvt.w = 1.0 - uvt.x - uvt.y;
-
-                if (all(greaterThanEqual(uvt, vec4(0.0))) && uvt.z < maxDist)
-                    return true;
-
-            }
-        }
-        else if (leaf < 0) // Leaf node of TLAS
-        {
-            vec4 r1 = texelFetch(transformsTex, ivec2((-leaf - 1) * 4 + 0, 0), 0).xyzw;
-            vec4 r2 = texelFetch(transformsTex, ivec2((-leaf - 1) * 4 + 1, 0), 0).xyzw;
-            vec4 r3 = texelFetch(transformsTex, ivec2((-leaf - 1) * 4 + 2, 0), 0).xyzw;
-            vec4 r4 = texelFetch(transformsTex, ivec2((-leaf - 1) * 4 + 3, 0), 0).xyzw;
-
-            mat4 transform = mat4(r1, r2, r3, r4);
-
-            rTrans.origin = vec3(inverse(transform) * vec4(r.origin, 1.0));
-            rTrans.direction = vec3(inverse(transform) * vec4(r.direction, 0.0));
-
-            // Add a marker. We'll return to this spot after we've traversed the entire BLAS
-            stack[ptr++] = -1;
-
-            index = leftIndex;
-            BLAS = true;
-            continue;
-        }
-        else
-        {
-            leftHit = AABBIntersect(texelFetch(BVH, leftIndex * 3 + 0).xyz, texelFetch(BVH, leftIndex * 3 + 1).xyz, rTrans);
-            rightHit = AABBIntersect(texelFetch(BVH, rightIndex * 3 + 0).xyz, texelFetch(BVH, rightIndex * 3 + 1).xyz, rTrans);
-
-            if (leftHit > 0.0 && rightHit > 0.0)
-            {
-                int deferred = -1;
-                if (leftHit > rightHit)
-                {
-                    index = rightIndex;
-                    deferred = leftIndex;
-                }
-                else
-                {
-                    index = leftIndex;
-                    deferred = rightIndex;
-                }
-
-                stack[ptr++] = deferred;
-                continue;
-            }
-            else if (leftHit > 0.)
-            {
-                index = leftIndex;
-                continue;
-            }
-            else if (rightHit > 0.)
-            {
-                index = rightIndex;
-                continue;
-            }
-        }
-        index = stack[--ptr];
-
-        // If we've traversed the entire BLAS then switch to back to TLAS and resume where we left off
-        if (BLAS && index == -1)
-        {
-            BLAS = false;
-
-            index = stack[--ptr];
-
-            rTrans.origin = r.origin;
-            rTrans.direction = r.direction;
-        }
-    }
-
-    return false;
-}
-
 vec3 CosineSampleHemisphere(float r1, float r2)
 {
     vec3 dir;
@@ -536,52 +306,11 @@ vec3 CosineSampleHemisphere(float r1, float r2)
     return dir;
 }
 
-float PowerHeuristic(float a, float b)
-{
-    float t = a * a;
-    return t / (b * b + t);
-}
-
 void Onb(in vec3 N, inout vec3 T, inout vec3 B)
 {
     vec3 up = abs(N.z) < 0.999 ? vec3(0, 0, 1) : vec3(1, 0, 0);
     T = normalize(cross(up, N));
     B = cross(N, T);
-}
-
-void SampleRectLight(in Light light, in vec3 surfacePos, inout LightSampleRec lightSampleRec)
-{
-    float r1 = rand();
-    float r2 = rand();
-
-    vec3 lightSurfacePos = light.position + light.u * r1 + light.v * r2;
-    lightSampleRec.direction = lightSurfacePos - surfacePos;
-    lightSampleRec.dist = length(lightSampleRec.direction);
-    float distSq = lightSampleRec.dist * lightSampleRec.dist;
-    lightSampleRec.direction /= lightSampleRec.dist;
-    lightSampleRec.normal = normalize(cross(light.u, light.v));
-    lightSampleRec.emission = light.emission * float(numOfLights);
-    lightSampleRec.pdf = distSq / (light.area * abs(dot(lightSampleRec.normal, lightSampleRec.direction)));
-}
-
-void SampleOneLight(in Light light, in vec3 surfacePos, inout LightSampleRec lightSampleRec)
-{
-    int type = int(light.type);
-
-    if (type == QUAD_LIGHT)
-        SampleRectLight(light, surfacePos, lightSampleRec);
-}
-
-vec3 EmitterSample(in Ray r, in State state, in LightSampleRec lightSampleRec, in BsdfSampleRec bsdfSampleRec)
-{
-    vec3 Le;
-
-    if (state.depth == 0)
-        Le = lightSampleRec.emission;
-    else
-        Le = PowerHeuristic(bsdfSampleRec.pdf, lightSampleRec.pdf) * lightSampleRec.emission;
-
-    return Le;
 }
 
 vec3 LambertSample(State state, vec3 V, vec3 N, inout vec3 L, inout float pdf)
@@ -605,69 +334,17 @@ vec3 LambertEval(State state, vec3 V, vec3 N, vec3 L, inout float pdf)
     return (1.0 / PI) * state.mat.baseColor * dot(N, L);
 }
 
-vec3 DirectLight(in Ray r, in State state)
-{
-    vec3 Li = vec3(0.0);
-    vec3 surfacePos = state.fhp + state.ffnormal * EPS;
-
-    BsdfSampleRec bsdfSampleRec;
-
-    // Analytic Lights 
-    if (numOfLights > 0)
-    {
-        LightSampleRec lightSampleRec;
-        Light light;
-
-        //Pick a light to sample
-        int index = int(rand() * float(numOfLights)) * 5;
-
-        // Fetch light Data
-        vec3 position = texelFetch(lightsTex, ivec2(index + 0, 0), 0).xyz;
-        vec3 emission = texelFetch(lightsTex, ivec2(index + 1, 0), 0).xyz;
-        vec3 u = texelFetch(lightsTex, ivec2(index + 2, 0), 0).xyz; // u vector for rect
-        vec3 v = texelFetch(lightsTex, ivec2(index + 3, 0), 0).xyz; // v vector for rect
-        vec3 params = texelFetch(lightsTex, ivec2(index + 4, 0), 0).xyz;
-        float radius = params.x;
-        float area = params.y;
-        float type = params.z;
-
-        light = Light(position, emission, u, v, radius, area, type);
-        SampleOneLight(light, surfacePos, lightSampleRec);
-
-        if (dot(lightSampleRec.direction, lightSampleRec.normal) < 0.0) 
-        {
-            Ray shadowRay = Ray(surfacePos, lightSampleRec.direction);
-            bool inShadow = AnyHit(shadowRay, lightSampleRec.dist - EPS);
-
-            if (!inShadow)
-            {
-                bsdfSampleRec.f = LambertEval(state, -r.direction, state.ffnormal, lightSampleRec.direction, bsdfSampleRec.pdf);
-
-                float weight = 1.0;
-                if (light.area > 0.0) // No MIS for distant light
-                    weight = PowerHeuristic(lightSampleRec.pdf, bsdfSampleRec.pdf);
-
-                if (bsdfSampleRec.pdf > 0.0)
-                    Li = weight * bsdfSampleRec.f * lightSampleRec.emission / lightSampleRec.pdf;
-            }
-        }
-    }
-
-    return Li;
-}
-
 vec4 PathTrace(Ray r)
 {
     vec3 radiance = vec3(0.0);
     vec3 throughput = vec3(1.0);
     State state;
-    LightSampleRec lightSampleRec;
     BsdfSampleRec bsdfSampleRec;
 
     for (int depth = 0; depth < 2; depth++)
     {
         state.depth = depth;
-        bool hit = ClosestHit(r, state, lightSampleRec);
+        bool hit = ClosestHit(r, state);
 
         if (!hit)
         {
@@ -676,14 +353,6 @@ vec4 PathTrace(Ray r)
         }
 
         state.mat.baseColor = texelFetch(materialsTex, ivec2(state.matID * 8, 0), 0).rgb;
-
-        if (state.isEmitter)
-        {
-            radiance += EmitterSample(r, state, lightSampleRec, bsdfSampleRec) * throughput;
-            break;
-        }
-
-        radiance += DirectLight(r, state) * throughput;
 
         bsdfSampleRec.f = LambertSample(state, -r.direction, state.ffnormal, bsdfSampleRec.L, bsdfSampleRec.pdf);
         throughput *= bsdfSampleRec.f / bsdfSampleRec.pdf;
